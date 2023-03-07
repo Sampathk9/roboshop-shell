d0bfaf57c7a3bda686c2033f80957820601e006f
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 00:36:07 +0800

    Catalog

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,32 @@
 script_location=$(pwd)
 
+#log is where log's are stored
+LOG=/tmp/roboshop.log
 echo -e "\e[35m Install Nginx\e[0m"
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+yum install nginx -y   &>>${LOG}
+echo $?
+
+
+
 echo -e "\e[35m Remove Nginx old content \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Download Frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+echo $?
+
+
+cd /usr/share/nginx/html &>>${LOG}
+
 echo -e "\e[35m Extract frontend content \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Copy roboshop Nginx config file\e[0m"
-cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+echo $?
+
 echo -e "\e[35m Started Nginx \e[0m"
 systemctl  restart nginx
+echo $?
