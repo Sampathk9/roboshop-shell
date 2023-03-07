da14c2750b83a7a6c13f40571f3ba13c081b63f6
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 01:10:00 +0800

    Exit output

--- catalogue.sh
+++ catalogue.sh
@@ -1,27 +1,144 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
 #stops where the error is
 #set -e
+echo -e "\e[35m Configuring Nodejs repo \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
 
-useradd roboshop
+echo -e "\e[35m Install Nodejs \e[0m"
+yum install nodejs -y &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Add Application User \e[0m"
+useradd roboshop &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
 #-p skips if directory exists if not it creates a directory
-mkdir -p /app
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
+mkdir -p /app &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Downloading App Content \e[0m"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>&{LOG}
+
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+echo -e "\e[35m Cleanup content \e[0m"
+rm -rf /app/* &>>&{LOG}
+
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Extracting Zip File \e[0m"
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>&{LOG}
 
-cd /app
-npm install
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
 
-cp ${script_location}/Files/catalogue.service /etc/systemd/system.catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
 
-cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
 
-mongo --host mongodb-dev.sampathkumar.online </app/schema/catalogue.js
+echo -e "\e[35m Installing Nodejs Dependencies \e[0m"
+cd /app &>>&{LOG}
+npm install &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Configuring Catalog service file \e[0m"
+cp ${script_location}/Files/catalogue.service /etc/systemd/system.catalogue.service &>>&{LOG}
+
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Reload System \e[0m"
+systemctl daemon-reload &>>&{LOG}
+
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Enable service \e[0m"
+systemctl enable catalogue &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m start catalogue service \e[0m"
+systemctl start catalogue &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Configuring mongodb \e[0m"
+cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+echo -e "\e[35m Installing Mongo client \e[0m"
+yum install mongodb-org-shell -y &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
+
+echo -e "\e[35m Load schema \e[0m"
+mongo --host mongodb-dev.sampathkumar.online </app/schema/catalogue.js &>>&{LOG}
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
--- frontend.sh
+++ frontend.sh
@@ -4,33 +4,69 @@ script_location=$(pwd)
 LOG=/tmp/roboshop.log
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y   &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
 
 
 
 echo -e "\e[35m Remove Nginx old content \e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
 
 echo -e "\e[35m Download Frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
 
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[35m Extract frontend content \e[0m"
 unzip /tmp/frontend.zip  &>>${LOG}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
+
 
 echo -e "\e[35m Copy roboshop Nginx config file\e[0m"
 cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo $?
+ if [ $? -eq 0 ]
+ then
+   echo success
+ else echo fail
+ fi
+
+
+
 
 echo -e "\e[35m Enabled Nginx \e[0m"
 systemctl  enable nginx
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
+
 
 echo -e "\e[35m Started Nginx \e[0m"
 systemctl  restart nginx
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+else echo fail
+fi
