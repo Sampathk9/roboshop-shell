79986257ae16774b8310b43d1bf166c307c67c07
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 02:30:47 +0800

    Functions with input's

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,21 @@
-script_location=$(pwd)
+source common.sh
 
+print_head "Copying MongoDB repo file"
 cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+status_check
 
+print_head "Install MondoDb"
 yum install mongodb-org -y
+status_check
 
+print_head "Updating MongoDB address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status_check
+
+print_head "Enable MongoDB"
 systemctl enable mongod
+status_check
+
+print_head "Restart MongoDB"
 systemctl restart mongod
+status_check
