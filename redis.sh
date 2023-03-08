75cbb0c551091dd5717d768946ec6293465cd530
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 13:36:02 +0800

    Catalog Service file modification

---
+++ redis.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head "setup Redis Repo"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
+status_check
+
+print_head "ENABLE Redis Package Stream"
+dnf module enable redis:remi-6.2 -y &>>${LOG}
+status_check
+
+print_head " Install Redis"
+yum install redis -y &>>${LOG}
+status_checks
+
+print_head "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>${LOG}
+status_check
+
+print_head "Enable Redis Service"
+systemctl enable redis &>>${LOG}
+status_check
+
+print_head "Start Redis Service"
+systemctl start redis &>>${LOG}
+status_check
