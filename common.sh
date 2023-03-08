890f70f774f3ed5868b12a24a7f0921b3fd32108
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 17:09:19 +0800

    Shipping Service

--- common.sh
+++ common.sh
@@ -150,3 +150,31 @@ MAVEN() {
 
   LOAD_SCHEMA
 }
+
+
+
+PYTHON() {
+
+
+   print_head "Install PYTHON"
+    yum install python36 gcc python3-devel -y &>>${LOG}
+    status_check
+
+  APP_PREREQ
+
+  print_head "Download Dependencies"
+  cd /app &>>${LOG}
+  pip3.6 install -r requirements.txt &>>${LOG}
+  status_check
+
+  print_head "Update Password's in Service File"
+  sed -i -e "s/roboshop_rabbitmq_password/${roboshop_rabbitmq_password}/" Files/${component}.service &>>{LOG}
+  status_check
+
+  SYSTEMD_SETUP
+
+  LOAD_SCHEMA
+
+
+
+}
