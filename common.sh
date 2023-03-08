e893ec9233b6c7c0f8a1db61a314b7e701d0ef8a
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 15:36:06 +0800

    Shipping Service

--- common.sh
+++ common.sh
@@ -79,7 +79,7 @@ SYSTEMD_SETUP() {
 LOAD_SCHEMA(){
   if [ ${schema_load} == "true" ]; then
 
-    if [ ${schema_type} == "mongo"]; then
+    if [ ${schema_type} == "mongo" ]; then
     echo -e "\e[31m Configuring mongodb \e[0m"
     cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
     status_check
@@ -93,13 +93,14 @@ LOAD_SCHEMA(){
     status_check
     fi
 
-   if [ ${schema_type} == "mysql"]; then
+   if [ ${schema_type} == "mysql"  ]; then
+
        echo -e "\e[31m Installing Mysql client \e[0m"
       yum install mysql -y &>>${LOG}
       status_check
 
       echo -e "\e[31m Load schema \e[0m"
-      mysql -h mysql-dev.samapthkumar.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+      mysql -h mysql-dev.sampathkumar.online -uroot -p${root_mysql_password} < /app/schema/${component}.sql  &>>${LOG}
       status_check
       fi
   fi
